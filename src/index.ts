28a6ffe8a216e69f6d6ad65c797b18463aa80e21
lei <oizhaolei@gmail.com>
2022-02-19 21:18:27 +0900

    split save

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
+import R from "ramda";
 import { faker } from "@faker-js/faker";
 import { createConnection } from "typeorm";
-import { Post } from "./entity/Post";
 import { Category } from "./entity/Category";
+import { Post } from "./entity/Post";
 
 // Note: The incoming request has too many parameters. The server supports a maximum of 2100 parameters. Reduce the number of parameters and resend the request.
-const chunk = 10;
-const repeat = 1000;
+const repeat = 10000;
 createConnection()
   .then(async (connection) => {
     const queryRunner = connection.createQueryRunner();
@@ -13,26 +13,29 @@ createConnection()
     await queryRunner.startTransaction();
 
     try {
-      for (var i = 0; i < chunk; ++i) {
-        const posts: Post[] = " "
-          .repeat(repeat)
-          .split("")
-          .map(() => {
-            const category1 = new Category();
-            category1.name = faker.commerce.productName();
+      const posts: Post[] = " "
+        .repeat(repeat)
+        .split("")
+        .map(() => {
+          const category1 = new Category();
+          category1.name = faker.commerce.productName();
 
-            const category2 = new Category();
-            category2.name = faker.commerce.productName();
+          const category2 = new Category();
+          category2.name = faker.commerce.productName();
 
-            const post = new Post();
-            post.title = faker.commerce.productName();
-            post.text = faker.commerce.productDescription();
-            post.categories = [category1, category2];
-            return post;
-          });
-        const saveResult = await queryRunner.manager.save(posts);
-        console.log("Post has been saved: ", saveResult.length);
-      }
+          const post = new Post();
+          post.title = faker.commerce.productName();
+          post.text = faker.commerce.productDescription();
+          post.categories = [category1, category2];
+          return post;
+        });
+      const saveResult = await Promise.all(
+        R.splitEvery(10, posts).map((post) =>
+          queryRunner.manager.save(Post, post)
+        )
+      );
+
+      console.log("Post has been saved: ", saveResult.flat());
 
       await queryRunner.commitTransaction();
 
